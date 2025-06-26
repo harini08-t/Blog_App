87a3d2cce12bd7c0122af5f4278e851a4b6311b8
harini08-t <harinithangavel0810@gmail.com>
2025-06-26 21:24:15 +0530

    add show more button

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -7,6 +7,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 
 export default function DashPost() {
   const {currentUser}=useSelector((state)=>state.user);
   const [userPosts,setUserPosts]=useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(()=>{
     const fetchPosts =async ()=>{
@@ -15,6 +16,9 @@ export default function DashPost() {
         const data=await res.json()
         if(res.ok){
           setUserPosts(data.posts)
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
         
       } catch (error) {
@@ -26,6 +30,23 @@ export default function DashPost() {
     }
 
   },[currentUser._id])
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'> 
@@ -83,6 +104,14 @@ export default function DashPost() {
               </TableBody>
             ))}
           </Table>
+          {showMore && ( 
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
