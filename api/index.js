b0790230b53a76cc951b2f111082c5e9b6b1f11d
harini08-t <harinithangavel0810@gmail.com>
2025-07-02 17:08:58 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import commentRoutes from './routes/comment.route.js'
+import path from 'path';
 dotenv.config();
 
  mongoose.connect(process.env.MONGO)
@@ -16,6 +17,7 @@ dotenv.config();
     console.log(err);
   });
 // const express = require('express')
+const __dirname = path.resolve();
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -29,6 +31,11 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use((err,req,res,next)=>{
   const statusCode = err.statusCode || 500;
